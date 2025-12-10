e6a59eb928a48a338f223c719b39938d1235373e
Joachem Trinidad <jtrinidad@vauldex.com>
2025-12-10 08:27:25 +0800

    first commit

---
+++ src/main/kotlin/com/example/activity_log_api/ActivityLogApiApplication.kt
@@ -0,0 +1,11 @@
+package com.example.activity_log_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ActivityLogApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<ActivityLogApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/activity_log_api/ActivityLogApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.activity_log_api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ActivityLogApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
